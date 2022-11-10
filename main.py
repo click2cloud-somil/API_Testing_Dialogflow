0b8a5bae80d971e84d6c8d0c59376e37a1dfbbd0
Somil Mehta <96865750+click2cloud-somil@users.noreply.github.com>
2022-11-10 15:09:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "How Are You"
+
+@app.route('/blog')
+def blog():
+    return ("open your blog")
+
+@app.route('/doctors')
+def get_doctors():
+    doctors = [
+        {'id':1, 'Name':'Somil Mehta', 'Contact Number': 7829359892, 'Address': 'Behind Jaiswal Chitramandir'},
+        {'id':2, 'Name':'Shreya Dubey','Contact Number': 9713259882,'Address': 'Near Mahila Bank'},
+        {'id':3, 'Name':'Abhijit Meshram','Contact Number': 8055071497,'Address':'Kamptee Road'},
+        {'id':4, 'Name':'Nishant Kohale','Contact Number':9834988102,'Address':'Near Nursing Home'}
+        ]
+    return jsonify(doctors)
+
+
+app.run(port=5000,debug=True)
